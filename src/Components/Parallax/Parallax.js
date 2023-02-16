0906f81b916c41e9c40d5e73a13621bcdf852b21
skylineent <skyline.enterprises55@gmail.com>
2023-02-16 22:12:52 +0530

    changes for reveal

--- src/Components/Parallax/Parallax.js
+++ src/Components/Parallax/Parallax.js
@@ -1,52 +1,48 @@
 import React from "react";
 import "./Parallax.scss";
-import Flip from 'react-reveal/Flip';
-import CountUp, { startAnimation } from 'react-countup';
-
+// import Flip from 'react-reveal/Flip';
+import CountUp, { startAnimation } from "react-countup";
 
 function Parallax() {
-
-
-    return (
-        <>
-            <div className="parallax-main-container" id="para">
-                <div className="parallax">
-                    <div className="para-content-container">
-                        <div className="para-sub-container1">
-                            <div className="para-tag">Helping Find Solution For Customer Since 1990</div>
-                            <div className="para-sub-tag">Placerat nullam cum a magnis ultrices mattis condimentum cubilia lacinia lacus vel aenean.</div>
-                        </div>
-                        <div className="para-sub-container2" >
-                            <div className="para-counter-box">
-                                <Flip right>
-                                    <div className="counter-box">
-                                        2050 +
-                                    </div>
-                                </Flip>
-                                <div className="para-counter-tag">Happy Clients</div>
-                            </div>
-                            <div className="para-counter-box">
-                                <Flip right>
-                                    <div className="counter-box">
-                                        70 +
-                                    </div>
-                                </Flip>
-                                <div className="para-counter-tag">Investica Network</div>
-                            </div>
-                            <div className="para-counter-box">
-                                <Flip right>
-                                    <div className="counter-box">
-                                        1650 +
-                                    </div>
-                                </Flip>
-                                <div className="para-counter-tag">Loan Approval</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="parallax-main-container" id="para">
+        <div className="parallax">
+          <div className="para-content-container">
+            <div className="para-sub-container1">
+              <div className="para-tag">
+                Helping Find Solution For Customer Since 1990
+              </div>
+              <div className="para-sub-tag">
+                Placerat nullam cum a magnis ultrices mattis condimentum cubilia
+                lacinia lacus vel aenean.
+              </div>
             </div>
-        </>
-    )
+            <div className="para-sub-container2">
+              <div className="para-counter-box">
+                <div>
+                  <div className="counter-box">2050 +</div>
+                </div>
+                <div className="para-counter-tag">Happy Clients</div>
+              </div>
+              <div className="para-counter-box">
+                <div>
+                  <div className="counter-box">70 +</div>
+                </div>
+                <div className="para-counter-tag">Investica Network</div>
+              </div>
+              <div className="para-counter-box">
+                <div>
+                  <div className="counter-box">1650 +</div>
+                </div>
+                <div className="para-counter-tag">Loan Approval</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Parallax;
